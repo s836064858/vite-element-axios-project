ef86f62767e76929df26d29ccd970d3c11d7eb55
s836064858 <836064858@qq.com>
2022-11-09 15:21:06 +0800

    fix: 修改组件所在文件夹及添加文档

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+const app = createApp(App)
+//引入路由
+import router from './router'
+app.use(router)
+//引入element-plus
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
-import router from './router'
-
-import Icon from './assets/js/icon'
-
-const app = createApp(App)
-app.use(router)
 app.use(ElementPlus, {
   locale: zhCn,
 })
+//引入全局Icon组件
+import Icon from './components/icon'
 app.component('Icon', Icon)
+
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,12 @@
 <!--
  * @Author: your name
  * @Date: 2021-12-16 09:18:47
- * @LastEditTime: 2022-09-30 13:30:28
+ * @LastEditTime: 2022-11-09 15:19:17
  * @LastEditors: 荛子
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vite-element-axios-project/src/views/Home.vue
 -->
-<template>
-  首页
-  <!-- elIcon实例 已全局配置 name输入对应elIcon即可 -->
-  <Icon name="Search"></Icon>
-</template>
+<template>首页</template>
 <script setup>
 //接口请求事例
 // import { onMounted } from 'vue'
