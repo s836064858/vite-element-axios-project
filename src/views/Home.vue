6f2ccda81ba87e9f23cbff99fa536461c106f239
s836064858 <836064858@qq.com>
2024-07-08 11:30:04 +0800

    feat: 添加luckySheet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-12-16 09:18:47
- * @LastEditTime: 2024-07-05 16:42:17
+ * @LastEditTime: 2024-07-08 11:29:24
  * @LastEditors: 荛子
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vite-element-axios-project/src/views/Home.vue
@@ -76,11 +76,22 @@ import MergeTable from '@components/mergeTable.vue'
 </script>
 <style lang="scss" scoped></style> -->
 
-<template>
+<!-- <template>
   <univer-excel></univer-excel>
 </template>
 <script setup>
 import { onMounted, ref } from 'vue'
+import univerExcel from '@components/univerExcel.vue'
+</script>
+<style lang="scss" scoped></style> -->
+
+<template>
+  <luckySheet></luckySheet>
+  <!-- <univer-excel></univer-excel> -->
+</template>
+<script setup>
+import { onMounted, ref } from 'vue'
+import luckySheet from '@components/luckySheet.vue'
 import univerExcel from '@components/univerExcel.vue'
 </script>
 <style lang="scss" scoped></style>
